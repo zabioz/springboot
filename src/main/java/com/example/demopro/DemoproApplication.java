1992c45ff312864233dca7d3992b8a27bacd8d7e
zabioz <zabioz00@gmail.com>
2020-01-07 07:40:33 +0900

    환경구축, jsp적용중

---
+++ src/main/java/com/example/demopro/DemoproApplication.java
@@ -0,0 +1,13 @@
+package com.example.demopro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoproApplication.class, args);
+	}
+
+}
